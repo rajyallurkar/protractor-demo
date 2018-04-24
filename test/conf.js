40703bd3d8a7f749f949a39f13a70d175588d509
Yallurkar, Raju (US - Bengaluru) <ryallurkar@deloitte.com>
2018-04-24 11:16:46 +0200

    first commit with typescript changes

--- test/conf.js
+++ test/conf.js
@@ -1,14 +1,29 @@
 // Tests for the calculator.
+
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+const baseUrl = 'http://localhost:3456';
+
 exports.config = {
   directConnect: true,
 
   framework: 'jasmine2',
-
+  baseUrl: baseUrl,
   specs: [
-    'spec.js'
+    'spec.ts'
   ],
 
   capabilities: {
     'browserName': 'chrome'
   },
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: 'target/screenshots'
+      })
+    );
+    require('ts-node').register({
+      project: 'test/tsconfig.json'
+    });
+  }
+
 };
