80ed6b9e6a9b9ce71dc30bbd80e13f43e6cc1a8b
Yallurkar, Raju (US - Bengaluru) <ryallurkar@deloitte.com>
2018-05-10 13:15:34 +0200

    fix:  firefox config GeckoDriverService

--- howtos/setFirefoxProfile/conf.js
+++ howtos/setFirefoxProfile/conf.js
@@ -1,13 +1,19 @@
 var helper = require('./helper.js');
+const seleniumAddress = process.env.E2E_SELENIUM_ADDRESS || 'http://localhost:4444/wd/hub';
+
 
 exports.config = {
-  directConnect: true,
+  directConnect: false,
+  seleniumAddress: seleniumAddress,
 
   specs: [
     'spec.js'
   ],
 
   framework: 'jasmine2',
-
-  getMultiCapabilities: helper.getFirefoxProfile
+  
+  capabilities: {
+    'browserName': 'firefox',
+    'marionette': true,
+  },
 };
